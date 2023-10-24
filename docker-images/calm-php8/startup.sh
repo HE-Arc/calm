d1ba0e949c8bcc82f80e97a6da0c199ee825df4f
ylked <nima@dekhli.ch>
2023-10-24 10:53:22 +0200

    update docker startup script to run vite

--- docker-images/calm-php8/startup.sh
+++ docker-images/calm-php8/startup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 cd /files
 /usr/local/bin/composer install
-/usr/bin/php artisan serve --host 0.0.0.0
+/usr/bin/npm install
+
+/usr/bin/php artisan serve --host 0.0.0.0&
+/usr/bin/npm run dev
