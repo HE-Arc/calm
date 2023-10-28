4fe3067f473e316559f5abe2ce6741e3086ba8d1
ylked <nima@dekhli.ch>
2023-10-28 21:57:43 +0200

    fix env fetching method

--- calm-webserver/app/Providers/AppServiceProvider.php
+++ calm-webserver/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\App;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(config('APP_ENV') !== 'local'){
+        if(!App::environment('local')){
             URL::forceScheme('https');
         }
     }
